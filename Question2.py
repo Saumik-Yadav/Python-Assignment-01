1bb24c8e07acb1d995fcaaa7d875e45c8b97f0c0
Saumik-Yadav <saumik.2025ug3009@iiitranchi.ac.in>
2025-12-28 10:00:41 +0530

    Create Question2.py

---
+++ Question2.py
@@ -0,0 +1,11 @@
+#Code:
+lst= [12,24,36,48,60]
+Sum = 0
+count=0
+for i in lst:
+    Sum= Sum + i
+    count+= 1
+avg= Sum/count
+print(f'the average of the elements of the given list is {avg}')
+#Output:
+#the average of the elements of the given list is 36.0
